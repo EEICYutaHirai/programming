56fa20d616d8d957b7908e91d3a4569e7fd470cb
EEICYutaHirai <paperbackprogrammer@gmail.com>
2018-09-14 12:10:57 +0900

    87

---
+++ atcoder/BegginerContest/87/A.cpp
@@ -0,0 +1,36 @@
+/*
+title:
+
+url:
+*/
+
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <functional>
+#include <queue>
+#include <limits>
+#include <string>
+#include <utility>
+#include <cstring>
+#include <map>
+#include <cmath>
+#include <iterator>
+
+using namespace std;
+
+#define REP(i, n) for (int i = 0; i < (n); i++)
+#define INF 1000000007
+
+typedef pair<int, int> pint;
+typedef long long ll;
+
+int main()
+{
+    int x, a, b;
+    cin >> x >> a >> b;
+    x -= a;
+    x %= b;
+    cout << x << endl;
+}
