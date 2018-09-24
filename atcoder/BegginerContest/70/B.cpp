be143555e95595d071b0e3c6547e9bcfd3c23247
EEICYutaHirai <033imanaya@gmail.com>
2018-09-25 01:49:53 +0900

    70

---
+++ atcoder/BegginerContest/70/B.cpp
@@ -0,0 +1,35 @@
+/*
+title:
+
+url:
+*/
+
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <functional>
+#include <queue>
+#include <limits>
+#include <string>
+#include <utility>
+#include <cstring>
+#include <map>
+#include <cmath>
+#include <iterator>
+
+using namespace std;
+
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define INF 1000000007
+
+typedef pair<int, int> pint;
+typedef long long ll;
+
+int main()
+{
+    int A, B, C, D;
+    cin >> A >> B >> C >> D;
+    cout << max(0, min(B, D) - max(A, C)) << endl;
+    return 0;
+}
